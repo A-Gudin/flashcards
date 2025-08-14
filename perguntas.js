054d92ab5f2ac20ddc0c8d43b6024a479709b695
A-Gudin <gudin.ana@escola.pr.gov.br>
2025-08-14 15:31:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,43 @@
+criaCartao(
+    'Biologia',
+    'Qual a unidade básica da vida?',
+    'Célula.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o processo pelo qual as plantas produzem seu próprio alimento?',
+    'Fotossíntese.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o nome do açúcar encontrado no sangue?',
+    'Glicose.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o gás que os humanos inalam?',
+    'Oxigênio.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a função principal dos glóbulos vermelhos?',
+    'Transportar oxigênio.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o nome do ácido encontrado no estômago que ajuda na digestão?',
+    'Ácido clorídrico.'
+)
+
+
+
+
+
+
+
+
